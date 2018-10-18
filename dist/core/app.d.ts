af1b77aa55a2af2917f271747c3076155a1f8733
Fang Yi <yifang@faceunity.com>
2018-10-18 18:15:05 +0800

    ci test

--- dist/core/app.d.ts
+++ dist/core/app.d.ts
@@ -1,6 +1,6 @@
-import 'reflect-metadata';
-import Koa from 'koa';
-import { options } from './type/opts';
+import "reflect-metadata";
+import Koa from "koa";
+import { options } from "./type/opts";
 export declare class Misc extends Koa {
     server: any;
     constructor(opts: options);
--- dist/core/app.js
+++ dist/core/app.js
@@ -19,7 +19,7 @@ const read_pkg_up_1 = __importDefault(require("read-pkg-up"));
 class Misc extends koa_1.default {
     constructor(opts) {
         const pack = read_pkg_up_1.default.sync().pkg;
-        log_1.logger.info('project:', pack.name);
+        log_1.logger.info("project:", pack.name);
         super();
         if (opts.body) {
             this.use(koa_body_1.default(opts.body));
@@ -29,7 +29,7 @@ class Misc extends koa_1.default {
         }
         this.keys = opts.keys;
         if (opts.cors) {
-            log_1.logger.success(`cors:${opts.cors}`);
+            log_1.logger.success(`cors:${JSON.stringify(opts.cors)}`);
             this.use(cors_1.default(opts.cors));
         }
         if (opts.session) {
@@ -40,44 +40,44 @@ class Misc extends koa_1.default {
         }
         let routerPath = [];
         if (opts.routerpath) {
-            glob_1.default.sync(path_1.join(opts.routerpath, './**/*.*{ts,js}')).forEach((item) => {
+            glob_1.default.sync(path_1.join(opts.routerpath, "./**/*.*{ts,js}")).forEach(item => {
                 if (require(item).default) {
                     try {
-                        let router = new (require(item).default)();
+                        let router = new (require(item)).default();
                         if (router instanceof koa_router_1.default) {
                             routerPath.push(item);
                             this.use(router.routes()).use(router.allowedMethods());
                         }
                         else {
-                            log_1.logger.error('router is not a Router instance:', item);
+                            log_1.logger.error("router is not a Router instance:", item);
                         }
                     }
                     catch (e) {
-                        log_1.logger.error('router load failed:', item);
+                        log_1.logger.error("router load failed:", item);
                     }
                 }
                 else {
-                    log_1.logger.error('router without default export', item);
+                    log_1.logger.error("router without default export", item);
                 }
             }, this);
         }
-        if (opts.protocol == 'http') {
+        if (opts.protocol == "http") {
             this.server = http_1.default.createServer(this.callback()).listen(opts.port);
         }
-        else if (opts.protocol == 'https') {
+        else if (opts.protocol == "https") {
             this.server = https_1.default.createServer(opts.tls, this.callback()).listen(opts.port);
         }
         else {
-            log_1.logger.error('lack of protocol');
+            log_1.logger.error("lack of protocol");
         }
         if (opts.routerpath) {
-            log_1.logger.success('Router Loading:', routerPath.map((item) => {
-                return item.split('/').pop();
+            log_1.logger.success("Router Loading:", routerPath.map(item => {
+                return item.split("/").pop();
             }));
         }
-        log_1.logger.success('NODE_ENV:' + process.env.NODE_ENV);
-        log_1.logger.success('server start at:' + opts.port);
-        log_1.logger.success('protocol:', opts.protocol);
+        log_1.logger.success("NODE_ENV:" + process.env.NODE_ENV);
+        log_1.logger.success("server start at:" + opts.port);
+        log_1.logger.success("protocol:", opts.protocol);
     }
 }
 exports.Misc = Misc;
