f0bb3287e582a491a45831b17110507c09db660c
Fang Yi <yifang@faceunity.com>
2018-12-18 18:49:47 +0800

    update default router prefix

--- dist/decorator/Http.d.ts
+++ dist/decorator/Http.d.ts
@@ -3,4 +3,4 @@ export declare function POST(url: string): MethodDecorator;
 export declare function GET(url: string): MethodDecorator;
 export declare function DELETE(url: string): MethodDecorator;
 export declare function PUT(url: string): MethodDecorator;
-export declare function Controller(baseurl?: string): ClassDecorator;
+export declare function Controller(prefix?: string): ClassDecorator;
