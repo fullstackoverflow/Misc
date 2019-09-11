0a243a3216ade1fbbd20f6cf4e45e9336e4dfd20
Fang Yi <yx136649111@qq.com>
2019-09-11 15:01:06 +0800

    update

--- lib/util/config.ts
+++ lib/util/config.ts
@@ -8,37 +8,37 @@ class Config {
 	private static _instance: any;
 
 	static get instance() {
-		if (this.path === undefined) {
+		if (Config.path === undefined) {
 			throw new Error("Config path is not init, set path first");
 		}
-		const default_path = existsSync(resolve(this.path, `./default.ts`)) ? resolve(this.path, `./default.ts`) : resolve(this.path, `./default.js`);
-		const config_path = existsSync(resolve(this.path, `./${process.env.NODE_ENV}.ts`)) ? resolve(this.path, `./${process.env.NODE_ENV}.ts`) : resolve(this.path, `./${process.env.NODE_ENV}.js`);;
-		if (this._instance == undefined) {
+		const default_path = existsSync(resolve(Config.path, `./default.ts`)) ? resolve(Config.path, `./default.ts`) : resolve(Config.path, `./default.js`);
+		const config_path = existsSync(resolve(Config.path, `./${process.env.NODE_ENV}.ts`)) ? resolve(Config.path, `./${process.env.NODE_ENV}.ts`) : resolve(Config.path, `./${process.env.NODE_ENV}.js`);;
+		if (Config._instance == undefined) {
 			if (existsSync(default_path) && existsSync(config_path)) {
-				this._instance = Object.assign({}, require(default_path).default, require(config_path).default);
+				Config._instance = Object.assign({}, require(default_path).default, require(config_path).default);
 				logger.success(`Config file load: ${process.env.NODE_ENV}.ts(merged default.ts)`);
 			} else if (existsSync(config_path)) {
-				this._instance = require(config_path).default;
+				Config._instance = require(config_path).default;
 				logger.success(`Config file load: ${process.env.NODE_ENV}.ts`);
 			} else {
 				throw new Error(`Config file ${config_path} load failed, file not exist`);
 			}
-			watch(this.path, {
+			watch(Config.path, {
 				ignored: /(^|[\/\\])\../,
 				persistent: true
 			}).on("change", () => {
 				delete require.cache[config_path];
 				if (existsSync(default_path)) {
 					delete require.cache[default_path];
-					this._instance = Object.assign({}, require(default_path).default, require(config_path).default);
+					Config._instance = Object.assign({}, require(default_path).default, require(config_path).default);
 					logger.success(`Config file load: ${process.env.NODE_ENV}.ts(merged default.ts)`);
 				} else {
-					this._instance = require(config_path).default;
+					Config._instance = require(config_path).default;
 					logger.success("Config file reload:", `${process.env.NODE_ENV}.ts`);
 				}
 			});
 		}
-		return this._instance;
+		return Config._instance;
 	}
 }
 
