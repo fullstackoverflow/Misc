a3f0dfb5204dd1c1a29e3d3638c3aeac7572a327
Fang Yi <yx136649111@qq.com>
2019-09-11 15:01:26 +0800

    update

--- dist/util/config.js
+++ dist/util/config.js
@@ -6,41 +6,41 @@ const log_1 = require("./log");
 const fs_1 = require("fs");
 class Config {
     static get instance() {
-        if (this.path === undefined) {
+        if (Config.path === undefined) {
             throw new Error("Config path is not init, set path first");
         }
-        const default_path = fs_1.existsSync(path_1.resolve(this.path, `./default.ts`)) ? path_1.resolve(this.path, `./default.ts`) : path_1.resolve(this.path, `./default.js`);
-        const config_path = fs_1.existsSync(path_1.resolve(this.path, `./${process.env.NODE_ENV}.ts`)) ? path_1.resolve(this.path, `./${process.env.NODE_ENV}.ts`) : path_1.resolve(this.path, `./${process.env.NODE_ENV}.js`);
+        const default_path = fs_1.existsSync(path_1.resolve(Config.path, `./default.ts`)) ? path_1.resolve(Config.path, `./default.ts`) : path_1.resolve(Config.path, `./default.js`);
+        const config_path = fs_1.existsSync(path_1.resolve(Config.path, `./${process.env.NODE_ENV}.ts`)) ? path_1.resolve(Config.path, `./${process.env.NODE_ENV}.ts`) : path_1.resolve(Config.path, `./${process.env.NODE_ENV}.js`);
         ;
-        if (this._instance == undefined) {
+        if (Config._instance == undefined) {
             if (fs_1.existsSync(default_path) && fs_1.existsSync(config_path)) {
-                this._instance = Object.assign({}, require(default_path).default, require(config_path).default);
+                Config._instance = Object.assign({}, require(default_path).default, require(config_path).default);
                 log_1.logger.success(`Config file load: ${process.env.NODE_ENV}.ts(merged default.ts)`);
             }
             else if (fs_1.existsSync(config_path)) {
-                this._instance = require(config_path).default;
+                Config._instance = require(config_path).default;
                 log_1.logger.success(`Config file load: ${process.env.NODE_ENV}.ts`);
             }
             else {
                 throw new Error(`Config file ${config_path} load failed, file not exist`);
             }
-            chokidar_1.watch(this.path, {
+            chokidar_1.watch(Config.path, {
                 ignored: /(^|[\/\\])\../,
                 persistent: true
             }).on("change", () => {
                 delete require.cache[config_path];
                 if (fs_1.existsSync(default_path)) {
                     delete require.cache[default_path];
-                    this._instance = Object.assign({}, require(default_path).default, require(config_path).default);
+                    Config._instance = Object.assign({}, require(default_path).default, require(config_path).default);
                     log_1.logger.success(`Config file load: ${process.env.NODE_ENV}.ts(merged default.ts)`);
                 }
                 else {
-                    this._instance = require(config_path).default;
+                    Config._instance = require(config_path).default;
                     log_1.logger.success("Config file reload:", `${process.env.NODE_ENV}.ts`);
                 }
             });
         }
-        return this._instance;
+        return Config._instance;
     }
 }
 exports.Config = Config;
