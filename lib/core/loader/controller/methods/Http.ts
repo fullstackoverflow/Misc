94f99409f48837e676b63bcc7755777d80786fe7
Fang Yi <yx136649111@qq.com>
2022-03-14 15:54:20 +0800

    fix:修复无参路由错误

--- lib/core/loader/controller/methods/Http.ts
+++ lib/core/loader/controller/methods/Http.ts
@@ -11,7 +11,7 @@ export class Http implements MethodLoader {
 		const path = Reflect.getMetadata(ControllerType.PATH, fn);
 		const method = Reflect.getMetadata(ControllerType.METHOD, fn);
 		const func = async (ctx: Context, next: Function) => {
-			let parameterIndexs: { type: ParameterDecoratorType, index: number }[] = Reflect.getOwnMetadata(Parameters, prototype, key);
+			let parameterIndexs: { type: ParameterDecoratorType, index: number }[] = Reflect.getOwnMetadata(Parameters, prototype, key) || [];
 			const paramters = parameterIndexs.sort((a, b) => a.index - b.index).map(item => {
 				return RequestContext.getInstance().ContextCache.get(ctx).get(item.type);
 			});
